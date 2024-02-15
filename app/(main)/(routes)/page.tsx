6e9b4a73490f7e47bb984112211311c16c652cca
vicky <vickykumarsunilja@gmail.com>
2024-02-15 14:55:55 +0530

    Added Dark mode themes

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,9 +1,11 @@
+import {DarkModeToggle} from "@/components/ui/darkmode";
 import {UserButton} from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
       <UserButton afterSignOutUrl="/" />
+      <DarkModeToggle />
     </div>
   );
 }
