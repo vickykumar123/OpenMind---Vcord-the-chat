dbd0cd035c5f2b8da8b6bf0a3eae828c0ddee222
vicky <vickykumarsunilja@gmail.com>
2024-03-09 15:49:06 +0530

    Completed chat header

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,9 +1,45 @@
+import ChatHeader from "@/components/chat/ChatHeader";
+import {currentUser} from "@/lib/current-user";
+import {db} from "@/lib/db";
+import {redirectToSignIn} from "@clerk/nextjs";
+import {redirect} from "next/navigation";
+
 interface ChannelIdPageProps {
   params: {
+    serverId: string;
     channelId: string;
   };
 }
 
-export default function ChannelIdPage({params}: ChannelIdPageProps) {
-  return <div>Channel {params.channelId} </div>;
+export default async function ChannelIdPage({params}: ChannelIdPageProps) {
+  const profile = await currentUser();
+
+  if (!profile) return redirectToSignIn();
+
+  const channel = await db.channel.findUnique({
+    where: {
+      id: params.channelId,
+    },
+  });
+
+  const member = await db.member.findFirst({
+    where: {
+      serverId: params.serverId,
+      profileId: profile.id,
+    },
+  });
+
+  if (!channel || !member) {
+    return redirect("/");
+  }
+
+  return (
+    <div className="bg-white dark:bg-[#313338] flex-col h-full">
+      <ChatHeader
+        serverId={channel.serverId}
+        name={channel.name}
+        type="channel"
+      />
+    </div>
+  );
 }
